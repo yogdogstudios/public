7909f1e1f7233ae85f7a0a862555a1af17ec2996
yogdogstudios <134594820+yogdogstudios@users.noreply.github.com>
2023-10-14 18:11:55 +0000

    Removed cookies function

--- colormode-toggle/colormode.js
+++ colormode-toggle/colormode.js
@@ -1,25 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function setCookie(name, value, days) {
-        let expires = "";
-        if (days) {
-            let date = new Date();
-            date.setTime(date.getTime() + (days*24*60*60*1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires;
-    }
-
-    function getCookie(name) {
-        let nameEQ = name + "=";
-        let ca = document.cookie.split(';');
-        for(let i=0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-
     function toggleColorMode() {
         // Get all elements with the attribute color-mode
         const elements = document.querySelectorAll('[color-mode]');
@@ -30,17 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // Loop through each element and set the attribute value
         elements.forEach(element => {
             element.setAttribute('color-mode', newMode);
-        });
-
-        // Store the user's choice in a cookie for 30 days
-        setCookie('colorModePreference', newMode, 30);
-    }
-
-    // Set the initial color mode based on the cookie
-    const preferredMode = getCookie('colorModePreference');
-    if (preferredMode) {
-        document.querySelectorAll('[color-mode]').forEach(element => {
-            element.setAttribute('color-mode', preferredMode);
         });
     }
 
