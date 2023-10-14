d6b27cb4c133179f963657a3dfbebdb7d4969157
yogdogstudios <134594820+yogdogstudios@users.noreply.github.com>
2023-10-14 17:55:48 +0000

    Removed cookie path from setCookie

--- colormode-toggle/colormode.js
+++ colormode-toggle/colormode.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
             date.setTime(date.getTime() + (days*24*60*60*1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+        document.cookie = name + "=" + (value || "") + expires;
     }
 
     function getCookie(name) {
